9302e6f7f020836381e9cc50ad6486cee5030d30
cleber.souza <ccs1201@gmail.com>
2024-08-20 17:00:04 -0300

    #20 - Remoção do QueryHint em MerchantRepository já que não estamos usando cache L2 e ajustes no application.properties

--- src/main/java/com/ccs/rabbitmqtests/domain/repositories/MerchantRepository.java
+++ src/main/java/com/ccs/rabbitmqtests/domain/repositories/MerchantRepository.java
@@ -2,11 +2,8 @@ package com.ccs.rabbitmqtests.domain.repositories;
 
 
 import com.ccs.rabbitmqtests.domain.models.entities.Merchant;
-import jakarta.persistence.QueryHint;
-import org.hibernate.jpa.HibernateHints;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,7 +11,6 @@ import java.util.Optional;
 @Repository
 public interface MerchantRepository extends JpaRepository<Merchant, Long> {
 
-    @QueryHints(@QueryHint(name = HibernateHints.HINT_CACHEABLE, value = "true"))
     @EntityGraph(attributePaths = {"mcc"})
     Optional<Merchant> findByName(String name);
 }

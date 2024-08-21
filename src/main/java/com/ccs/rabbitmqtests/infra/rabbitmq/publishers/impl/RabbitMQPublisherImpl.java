abf5ae6d61550dbe2cfc1075d1df4f233d79dd98
cleber.souza <ccs1201@gmail.com>
2024-08-21 17:17:31 -0300

    #24 - Refactoring da classe RabbitMQPublisherImpl

--- src/main/java/com/ccs/rabbitmqtests/infra/rabbitmq/publishers/impl/RabbitMQPublisherImpl.java
+++ src/main/java/com/ccs/rabbitmqtests/infra/rabbitmq/publishers/impl/RabbitMQPublisherImpl.java
@@ -4,6 +4,7 @@ import com.ccs.rabbitmqtests.domain.core.exceptions.AppRuntimeException;
 import com.ccs.rabbitmqtests.infra.rabbitmq.publishers.RabbitMQPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.MessagePostProcessor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
@@ -29,19 +30,14 @@ public class RabbitMQPublisherImpl implements RabbitMQPublisher {
     @Override
     public void sendMessage(String exchange, String routingKey, Object message) {
         log(exchange, routingKey, message);
-
-        rabbitTemplate.convertAndSend(exchange, routingKey, message, messageToSend -> {
-                    messageToSend.getMessageProperties().setHeader("timestamp", OffsetDateTime.now());
-                    messageToSend.getMessageProperties().setMessageId(UUID.randomUUID().toString());
-                    return messageToSend;
-                }
-        );
+        rabbitTemplate.convertAndSend(exchange, routingKey, message, setHeaders());
     }
 
     /**
      * <p>
      * Este método funciona como uma chamada RPC (remote procedure call) onde uma
-     * mensagem é publicada em uma fila do RabbitMQ e é esperada uma resposta.
+     * mensagem é publicada em uma fila do RabbitMQ e é esperada uma resposta de
+     * de forma síncrona.
      * </p>
      * <p>
      * O método envia uma mensagem para a fila especificado pela chave de roteamento {@code routingKey}
@@ -52,8 +48,8 @@ public class RabbitMQPublisherImpl implements RabbitMQPublisher {
      * O método retorna o objeto de resposta do tipo especificado em {@code responseClass}.
      * </p>
      *
-     * @param routingKey A RoutingKey da fila.
-     * @param message A mensagem que será enviada/publicada.
+     * @param routingKey    A RoutingKey da fila.
+     * @param message       A mensagem que será enviada/publicada.
      * @param responseClass O tipo de resposta esperada.
      * @return A response convertida para o tipo definido em responseClass.
      * @throws AppRuntimeException Se ocorrer um erro ao enviar ou receber a mensagem do RabbitMQ.
@@ -62,17 +58,22 @@ public class RabbitMQPublisherImpl implements RabbitMQPublisher {
         log(RabbitMQConstants.EXCHANGE_NAME, routingKey, message);
         try {
             Optional<Object> response = Optional.ofNullable(
-                    rabbitTemplate.convertSendAndReceive(RabbitMQConstants.EXCHANGE_NAME, routingKey, message, messageToSend -> {
-                        messageToSend.getMessageProperties().setHeader("timestamp", OffsetDateTime.now());
-                        messageToSend.getMessageProperties().setMessageId(UUID.randomUUID().toString());
-                        return messageToSend;
-                    })
+                    rabbitTemplate
+                            .convertSendAndReceive(RabbitMQConstants.EXCHANGE_NAME, routingKey, message, setHeaders())
             );
 
             return responseClass.cast(response.orElseThrow(() -> new AppRuntimeException("No response was received for message")));
         } catch (Exception e) {
             throw new AppRuntimeException("Error send and receive message to RabbitMQ", e);
         }
+    }
+
+    private static MessagePostProcessor setHeaders() {
+        return messagePostProcessor -> {
+            messagePostProcessor.getMessageProperties().setHeader("timestamp", OffsetDateTime.now());
+            messagePostProcessor.getMessageProperties().setMessageId(UUID.randomUUID().toString());
+            return messagePostProcessor;
+        };
     }
 
     private static void log(String exchange, String routingKey, Object message) {
